952f9707b46093678b6c110691926bb9a4d4935c
shivam1329 <123602103+shivam1329@users.noreply.github.com>
2023-02-02 00:05:27 +0530

    Add files via upload

---
+++ content_option.js
@@ -0,0 +1,125 @@
+const logotext = "SHIVAM";
+const meta = {
+  title: "Shivam Tiwari",
+  description:
+    "I’m Shivam Tiwari Digital Marketer & Devloper,currently working as a Freelance",
+};
+
+const introdata = {
+  title: "I’m Shivam Tiwari",
+  animated: {
+    first: "I love Digital Marketing",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description: "Recruiting great marketers should be your number one priority.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "a bit about my self",
+  aboutme:
+    "I am  a Digital Marketing professional who specializes in promoting and selling products or services through digital channels, such as social media, search engines, email, and websites. My responsibilities may include creating and executing marketing campaigns, analyzing data to determine campaign effectiveness, and identifying new opportunities for growth in the digital space. My goal is to reach and engage with their target audience to drive conversions and grow the business.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Digital Marketing Intern",
+    where: "Devhamlet",
+    date: "2020",
+  },
+  {
+    jobtitle: "Digital Marketing Lead",
+    where: "Devhamlet",
+    date: "2021",
+  },
+  {
+    jobtitle: "Digital Marketing Manager",
+    where: "Devhamlet",
+    date: "2022",
+  },
+];
+
+const skills = [
+  {
+    name: "SMM",
+    value: 90,
+  },
+  {
+    name: "CRM",
+    value: 85,
+  },
+  {
+    name: "Data Analysis",
+    value: 80,
+  },
+  {
+    name: "Content Creator",
+    value: 80,
+  },
+  {
+    name: "SEO",
+    value: 85,
+  },
+  {
+    name: "Designing",
+    value: 90,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description: "Crafting seamless experiences, one design at a time.",
+  },
+  {
+    title: "Digital marketing",
+    description: "Connecting brands to consumers in the digital age.",
+  },
+  {
+    title: "Software Development",
+    description:
+      "Building solutions, powering progress.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://degreeminusone.com/wp-content/uploads/2022/11/Degree-Minus-One-Logo-260x34.png",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://degreeminusone.com/",
+  },
+ 
+  
+ 
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "shivamtiwari1329@gmail.com",
+  YOUR_FONE: "+91 8527746012",
+  description:
+    "Any kind of Services related to technology, Contact me now.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/shivam1329",
+
+  linkedin: "https://www.linkedin.com/in/shivam-undefined-81a726263/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
